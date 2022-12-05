a8a79ba082488a1d9d6e5e09df647adb19bd5b7d
Alex Luque <alexluque29@gmail.com>
2022-12-05 14:02:13 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [boxStyle, setBoxStyle] = useState({
+    width: 80,
+    background: "#ff0000",
+  });
+
+  const [circleShape, setCircleShape] = useState({
+    width: 90,
+    background: "#11229C",
+  });
+
+  const [triangleShape, setTriangleShape] = useState({
+    width: 0,
+    background: "#57d111",
+  });
+
+  useEffect(() => {
+    if (currSqSize == currCiSize && currSqSize == currTrSize) {
+      alert("Los TAMAÑOS de las tres figuras son IGUALES");
+    }
+    if (currSqColor == currCiColor && currSqColor == currTrColor) {
+      alert("Los COLORES de las tres figuras son IGUALES");
+    }
+  }, [boxStyle, circleShape, triangleShape]);
+
+  let currSqSize = boxStyle.width;
+  let currSqColor = boxStyle.background;
+  let currCiSize = circleShape.width;
+  let currCiColor = circleShape.background;
+  let currTrSize = triangleShape.width;
+  let currTrColor = triangleShape.background;
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <h2 className="tarea">Módulo 2 - Tarea useState y useEffect</h2>
+      <div className="cont-square">
+        <div
+          style={{
+            width: `${boxStyle.width}px`,
+            height: `${boxStyle.width}px`,
+            background: boxStyle.background,
+          }}
+        ></div>
+        <input
+          type="number"
+          value={boxStyle.width}
+          style={{ width: 40 }}
+          onChange={(event) =>
+            setBoxStyle({ ...boxStyle, width: event.target.value })
+          }
+        />
+        <input
+          type="color"
+          value={boxStyle.background}
+          onChange={(event) =>
+            setBoxStyle({ ...boxStyle, background: event.target.value })
+          }
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="cont-circle">
+        <div
+          style={{
+            width: `${circleShape.width}px`,
+            height: `${circleShape.width}px`,
+            borderRadius: `${circleShape.width}px`,
+            background: circleShape.background,
+          }}
+        ></div>
+        <input
+          type="number"
+          value={circleShape.width}
+          style={{ width: 40 }}
+          onChange={(event) =>
+            setCircleShape({ ...circleShape, width: event.target.value })
+          }
+        />
+        <input
+          type="color"
+          value={circleShape.background}
+          onChange={(event) =>
+            setCircleShape({ ...circleShape, background: event.target.value })
+          }
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+      <div className="cont-triangle">
+        <div
+          style={{
+            width: `${triangleShape.width}px`,
+            height: `${triangleShape.width}px`,
+            borderLeft: `${50}px solid transparent`,
+            borderRight: `${50}px solid transparent`,
+            borderBottom: `${100}px solid ${triangleShape.background}`,
+          }}
+        ></div>
+        <input
+          type="number"
+          style={{ width: 40 }}
+          value={triangleShape.width}
+          onChange={(event) =>
+            setTriangleShape({ ...triangleShape, width: event.target.value })
+          }
+        />
+        <input
+          type="color"
+          value={triangleShape.background}
+          onChange={(event) =>
+            setTriangleShape({
+              ...triangleShape,
+              background: event.target.value,
+            })
+          }
+        />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
